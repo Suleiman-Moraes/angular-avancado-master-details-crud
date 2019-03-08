8e07db893086f6daf94e0410627f06fb3b5f02e6
Suleiman Moraes <suleimanmoraes@gmail.com>
2019-03-08 16:59:56 -0300

    Atualização

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -6,12 +6,17 @@ import { EntriesRoutingModule } from './entries-routing.module';
 import { EntryFormComponent } from './entry-form/entry-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
+import { CalendarModule } from "primeng/calendar";
+import { IMaskModule } from "angular-imask";
+
 @NgModule({
   declarations: [EntryListComponent, EntryFormComponent],
   imports: [
     CommonModule,
     EntriesRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CalendarModule,
+    IMaskModule
   ]
 })
 export class EntriesModule { }
