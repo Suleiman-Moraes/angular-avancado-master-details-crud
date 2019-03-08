926409ec0c1019f18757d4848557968092bfd038
Suleiman Moraes <suleimanmoraes@gmail.com>
2019-03-08 15:19:54 -0300

    Atualização

--- src/app/pages/categories/category-form/category-form.component.ts
+++ src/app/pages/categories/category-form/category-form.component.ts
@@ -4,6 +4,7 @@ import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { CategoryService } from '../shared/category.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
+import toastr from "toastr";
 
 @Component({
   selector: 'app-category-form',
@@ -34,6 +35,16 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked{
 
   ngAfterContentChecked(){
     this.setPageTitle();
+  }
+
+  submitForm(){
+    this.submittingForm = true;
+    if(this.currentAction == "new"){
+      this.createCategory();
+    }
+    else{//edit
+      this.updateCategory();
+    }
   }
 
   //PRIVATES METHODS
@@ -76,6 +87,46 @@ export class CategoryFormComponent implements OnInit, AfterContentChecked{
     else{
       const categoryName = this.category.name || ""
       this.pageTitle = "Editando Categoria: " + categoryName;
+    }
+  }
+
+  private createCategory(){
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+
+    this.categoryService.create(category)
+      .subscribe(
+        category => this.actionForSuccess(category),
+        error => this.actionsForError(error)
+      )
+  }
+  
+  private updateCategory(){
+    const category: Category = Object.assign(new Category(), this.categoryForm.value);
+    this.categoryService.update(category)
+      .subscribe(
+        category => this.actionForSuccess(category),
+        error => this.actionsForError(error)
+      )
+  }
+
+  private actionForSuccess(category: Category){
+    toastr.success("Solicitação processada com sucesso!");
+
+    //redirect/reload component page
+    this.router.navigateByUrl("categories", {skipLocationChange: true}).then(
+      () => this.router.navigate(["categories", category.id, "edit"])
+    )
+  }
+
+  private actionsForError(error){
+    toastr.error("Ocorreu um erro ao processar a sua solicitação!");
+    this.submittingForm = false;
+
+    if(error.status == 422){
+      this.serverErrorMessages = JSON.parse(error._body).errors;
+    }
+    else{
+      this.serverErrorMessages = ["Falha na comunicação com o servidor. Por favor, tente mais tarde."];
     }
   }
 }
