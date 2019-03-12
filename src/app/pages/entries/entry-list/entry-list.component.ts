58b4a4962d5e0b13a965beb63e9a5a9bd9cfdc53
Suleiman Moraes <suleimanmoraes@gmail.com>
2019-03-12 08:42:14 -0300

    Atualização

--- src/app/pages/entries/entry-list/entry-list.component.ts
+++ src/app/pages/entries/entry-list/entry-list.component.ts
@@ -1,3 +1,4 @@
+import { BaseResourceListComponent } from 'src/app/shared/components/base-resource-list/base-resource-list.component';
 import { EntryService } from './../shared/entry.service';
 import { Entry } from './../shared/entry.model';
 import { Component, OnInit } from '@angular/core';
@@ -7,27 +8,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './entry-list.component.html',
   styleUrls: ['./entry-list.component.css']
 })
-export class EntryListComponent implements OnInit {
+export class EntryListComponent extends BaseResourceListComponent<Entry> {
 
-  entries: Entry[] = [];
-
-  constructor(private entryService: EntryService) { }
-
-  ngOnInit() {
-    this.entryService.getAll().subscribe(
-      entries => this.entries = entries.sort((a,b) => b.id - a.id),
-      error => alert('Erro ao carregar a lista')
-    )
-  }
-
-  deleteEntry(entry){
-    const mustDelete = confirm('Deseja realmente excluir este item?');
-
-    if(mustDelete){
-      this.entryService.delete(entry.id).subscribe(
-        () => this.entries = this.entries.filter(element => element != entry),
-        () => alert("Erro ao tentar excluir!")
-      )
-    }
+  constructor(protected entryService: EntryService) {
+    super(entryService);
   }
 }
