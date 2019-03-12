8848fc98d6ab372a889e900042b37ef0ee901b09
Suleiman Moraes <suleimanmoraes@gmail.com>
2019-03-12 11:33:05 -0300

    Atualização

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,20 +5,28 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule, 
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase)
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDatabase),
+    RouterModule
   ],
   exports:[
+    //module
     BrowserModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule,
+
+    //component
+    NavbarComponent
   ]
 })
 export class CoreModule { }
